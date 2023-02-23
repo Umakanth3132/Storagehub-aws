8fbd05326d82a84af00e6c63c513856598547061
Umakanth <umakanthreddy.m@gmail.com>
2023-02-23 18:57:02 -0500

    Application done

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -23,9 +23,10 @@ const UserProfiles=()=>{
 
     return (
       <div key={index}>
-        {userProfile.userProfileId ? 
-        (<img src={`http://localhost:8080/api/v1/user-profile/${userProfile.userProfileId}/image/download`}/>)
-        :null}
+        {userProfile.userProfileId ? (
+        <img src={`http://localhost:8080/api/v1/user-profile/${userProfile.userProfileId}/image/download`}
+        />
+        ): null}
         
         <br/>
         <br/>
